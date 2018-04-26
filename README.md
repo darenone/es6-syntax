251194869490ce71db3d73c823601f2d0781f3e5
darenone <1291342008@qq.com>
2018-04-26 10:58:54 +0800

    ok

---
+++ README.md
@@ -0,0 +1,23 @@
+## es6-webpack
+
+### 安装
+
+首先下载源码
+
+```shell
+git clone https://github.com/cucygh/es6-webpack.git
+```
+
+然后安装
+
+```shell
+npm install
+npm install  webpack -g (如果本电脑原来没有安装过webpack,需要先全局安装)
+npm install  webpack-dev-server -g ()
+```
+
+### 运行
+
+```shell
+npm start
+```
---
+++ src/arrow-function.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+
+{
+  // ES3,ES5
+  var evens = [1, 2, 3, 4, 5];
+  var odds = evens.map(function(v) {
+    return v + 1
+  });
+  console.log(evens, odds);
+};
+{
+  // ES6
+  let evens = [1, 2, 3, 4, 5];
+  let odds = evens.map(v => v + 1);
+  console.log(evens, odds);
+} 
+{
+  // ES3,ES5声明一个类
+  var factory = function() {
+    this.a = 'a';
+    this.b = 'b';
+    this.c = {
+      a: 'a+',
+      b: function() {
+        return this.a //this指向该函数被调用的对象
+      }
+    }
+  }
+
+  console.log(new factory().c.b());
+};
+
+{
+  var factory = function() {
+    this.a = 'a';
+    this.b = 'b';
+    this.c = {
+      a: 'a+',
+      b: () => {
+        return this.a
+      }
+    }
+  }
+  console.log(new factory().c.b());
+}
